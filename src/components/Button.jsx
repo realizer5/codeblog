27d475b6cda74ef4c84485c622be76554ed4425c
realizer <ipreet35232@gmail.com>
2026-03-03 19:44:07 +0530

    fixed typo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Button({ children, type = "button", bgColor = "bg-blue-dark", textColor = "text-gray-light", className = "", ...props }) {
     return (
         <button type={type} className={`border border-slate-light cursor-pointer px-4 py-2 rounded-lg ${bgColor} ${textColor} duration-200 hover:bg-slate-light ${className}`} {...props}>
